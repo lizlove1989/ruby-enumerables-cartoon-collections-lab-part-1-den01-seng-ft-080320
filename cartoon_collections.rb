94e3a07759622593ea07407674ae248af08ee254
Liz Love <love.elizabeth1989@gmail.com>
2020-06-17 17:55:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,13 +2,13 @@ def greet_characters(array)
   array.each do |array|
    puts "Hello #{array}!"
  end
-  
-  # Use `each` to enumerate over the provided array
-  #
-  # Print a custom greeting for each element
+
 end
 
 def list_dwarves(array)
+  array.each_with_index do |array|
+    puts array
+  
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
