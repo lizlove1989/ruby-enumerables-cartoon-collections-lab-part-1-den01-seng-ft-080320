4c2b702a25c06d6de1610ff3949a68e4bc2588bd
Liz Love <love.elizabeth1989@gmail.com>
2020-06-17 18:04:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 
 def list_dwarves(array)
   array.each_with_index {|item, index|}
-  puts "#{index}"
+  puts "#{index}. #{index}"
   
   
   # Use `each_with_index` to enumerate over the provided array
